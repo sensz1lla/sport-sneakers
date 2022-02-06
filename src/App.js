58fe2960b75e2d3967ec458c32cbe95aa86c2740
sensz1lla <tony.n.music@gmai.com>
2022-02-07 00:16:35 +0200

    card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 3999,
+    imageUrl: '/img/sneakers/1.jpeg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 4299,
+    imageUrl: '/img/sneakers/2.jpeg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 2699,
+    imageUrl: '/img/sneakers/3.jpeg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 2999,
+    imageUrl: '/img/sneakers/4.jpeg'
+  },
+];
+
 function App() {
   return (
     <div className="app clear">
@@ -16,8 +39,17 @@ function App() {
           </div>
         </div>
 
-        <div className="cards d-flex justify-between">
-          <Card />
+        <div className="cards d-flex">
+          {arr.map((obj) => (
+
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+
+          ))}
         </div>
       </div>
     </div>
